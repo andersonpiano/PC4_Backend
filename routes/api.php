997b561293f66ba9a40002766b1e2a006c370096
Anderson <anderson@archlinux.com.br>
2021-06-24 14:10:55 -0300

    Implementando a interface de autenticação
    no model User.php foi carregado e implementado o JWTSubject e no Auth.php foi informado o driver para api JWT conforme documentação:
    
    https://jwt-auth.readthedocs.io/en/develop/quick-start/
    
    em routes/api.php foram criadas as rotas para login, logout, informações do usuário e renovação do token
    
    Criação do controller de autenticação com o comando:
    
    php artisan make:controller AuthController
    
    criado o usuario pela instrução
    
    php artisan tinker
    
    depois passando a variavel user
    
    $user = new App\Models\User();
    
    em seguida preencher os campos nome, email e senha
    
    $user->name = 'PC4'
    $user->email = 'login@pc4.com.br'
    $user->password = bcrypt('1234')
    $user->save()
    
    Criação dos metodos de autenticação no AuthController

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('PC4')->middleware('jwt.auth')->group(function () {
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+
+});
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+
